5774b37ead302e41cc82edb31cb46794740de4c7
Nai-12 <nabail349@gmail.com>
2023-10-20 21:00:09 +0700

    New Update

--- js/index.js
+++ js/index.js
@@ -4,3 +4,21 @@ const barList = document.querySelector(".bar input");
 barList.addEventListener("click", function () {
   navbar.classList.toggle("slide");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var kirimPesan = document.querySelector(".send");
+  var inputPesan = document.getElementById("chat");
+  var lihatPesan = document.querySelector(".chat-view");
+
+  kirimPesan.addEventListener("click", function () {
+    var kirimText = inputPesan.value;
+    if (kirimText.trim() !== "") {
+      var buatChat = document.createElement("li");
+      buatChat.textContent = kirimText;
+      lihatPesan.appendChild(buatChat);
+      inputPesan.value = "";
+      lihatPesan.scrollTop = lihatPesan.scrollHeight;
+      document.querySelector(".infoChat").style.display = "none";
+    }
+  });
+});
